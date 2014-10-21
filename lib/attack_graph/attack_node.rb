9fd323eccaa42994606dc9f0088ffef6f1c86315
Sarun Siriphantnon <deoxen0n2@gmail.com>
2014-10-21 20:05:11 +0700

    Add helper methods

--- lib/attack_graph/attack_node.rb
+++ lib/attack_graph/attack_node.rb
@@ -12,5 +12,20 @@ module AttackGraph
         service.service_name == service_name.to_s
       end
     end
+
+    def vulnerabilities
+      # TODO: make this Association and can be flatten
+      @vulnerabilities ||= services.map(&:vulnerabilities).map(&:to_a).flatten
+    end
+
+    def average_severity
+      severities = vulnerabilities.map(&:severity).map(&:to_f)
+      severities.reduce(:+) / severities.size
+    end
+
+    def reload
+      # TODO: super
+      @vulnerabilities = nil
+    end
   end
 end
