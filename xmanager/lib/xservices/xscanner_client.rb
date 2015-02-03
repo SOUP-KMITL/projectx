1f29896acf199e43da41b763fd13dae8e7a98edd
Sarun Siriphantnon <deoxen0n2@gmail.com>
2015-02-03 16:43:21 +0700

    Make XS Client read config from YAML

--- xmanager/lib/xservices/xscanner_client.rb
+++ xmanager/lib/xservices/xscanner_client.rb
@@ -2,7 +2,7 @@ require_relative '../../../lib/xservice'
 
 module XM
   class XScannerClient < XSV::XServiceClient::Base
-    # TODO: Read from YAML
-    base_uri 'localhost:3002'
+    settings = YAML.load_file(File.expand_path('../../../config.yml', __FILE__))['xscanner']
+    base_uri "#{settings['host']}:#{settings['port']}"
   end
 end
