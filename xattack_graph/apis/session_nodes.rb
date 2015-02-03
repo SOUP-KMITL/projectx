8f68d48a24d113f63386f1e62c4d34e6ee0fe14d
Sarun Siriphantnon <deoxen0n2@gmail.com>
2015-02-03 19:58:00 +0700

    Make session node updatable

--- xattack_graph/apis/session_nodes.rb
+++ xattack_graph/apis/session_nodes.rb
@@ -5,15 +5,38 @@ module XAttackGraphAPI
     end
 
     post '/sessions/?' do
-      session_node_property = {
+      session_node_properties = {
         created_at: Time.now.to_s,
-        task: params[:task]
+        task: params[:task],
+        progress: 0
       }
-      session_node = @neo.create_node(session_node_property)
-      @neo.add_label(session_node, 'Session')
-      session_node_property[:id] = session_node['self'].match(/data\/node\/(\d+)\Z/).captures.first.to_i
 
-      json session_node_property
+      session_node = @neo.create_node(session_node_properties)
+      @neo.add_label(session_node, 'Session')
+      session_node_properties[:id] = session_node['self'].match(/data\/node\/(\d+)\Z/).captures.first.to_i
+
+      json session_node_properties
+    end
+
+    get '/sessions/?' do
+      # TODO
+    end
+
+    get '/sessions/:session_id' do
+      session_node = @neo.get_node(params[:session_id])
+      session_hash = @neo.get_node_properties(session_node).merge({
+        id: params[:session_id]})
+
+      json session_hash
+    end
+
+    put '/sessions/:session_id' do
+      session_node = @neo.get_node(params[:session_id])
+      @neo.set_node_properties(session_node, params[:properties])
+      session_hash = @neo.get_node_properties(session_node).merge({
+        id: params[:session_id]})
+
+      json session_hash
     end
   end
 end
