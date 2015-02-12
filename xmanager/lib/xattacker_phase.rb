bb3cc6d30ee9bb415e6424bae9a7874a9068681f
Sarun Siriphantnon <deoxen0n2@gmail.com>
2015-02-12 11:33:24 +0700

    Register ssh_agent

--- xmanager/lib/xattacker_phase.rb
+++ xmanager/lib/xattacker_phase.rb
@@ -3,6 +3,6 @@ require_relative 'xservices/xattacker_client'
 module XM
   class XAttackerPhase < Phase
     client   XAttackerClient
-    register :hydra_adapter, :w3af_adapter, :nikto_adapter
+    register :hydra_adapter, :w3af_adapter, :nikto_adapter, :ssh_agent
   end
 end
