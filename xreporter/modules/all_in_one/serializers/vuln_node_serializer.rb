658928cd885f9e5447e26635b600a9920849f987
Sarun Siriphantnon <deoxen0n2@gmail.com>
2015-02-19 15:45:48 +0700

    Add cve_url field

--- xreporter/modules/all_in_one/serializers/vuln_node_serializer.rb
+++ xreporter/modules/all_in_one/serializers/vuln_node_serializer.rb
@@ -1,6 +1,6 @@
 class VulnNodeSerializer < ActiveModel::Serializer
   self.root = false
-  attributes :name, :xmodule, :description, :severity
+  attributes :name, :xmodule, :description, :severity, :cve_url
 
   def name
     object.name if object.respond_to?(:name)
@@ -12,6 +12,12 @@ class VulnNodeSerializer < ActiveModel::Serializer
 
   def description
     object.description if object.respond_to?(:description)
+  end
+
+  def cve_url
+    if object.respond_to?(:cve_url)
+      object.cve_url unless object.cve_url.empty?
+    end
   end
 end
 
